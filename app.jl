4bc9a188398ae489c5f5ce8ba037307eb8b25f58
Carlos Valero <valerocar@gmail.com>
2021-10-13 17:51:57 -0500

    First Commit

---
+++ app.jl
@@ -0,0 +1,55 @@
+using Pkg
+Pkg.activate(".")
+using PlotlyJS
+using Dash
+
+plot_width = 500
+plot_height = 500
+
+sd = 1.1
+layout = Layout(xaxis_title="x", yaxis_title="y",
+    xaxis_range=[-sd,sd],yaxis_range=[-sd,sd], title="Lissajous Curve",
+    width=plot_width, height=plot_height)
+
+l = 3    
+t = LinRange(0.0,l*2*pi,500)
+
+function myplot(ω1,ω2)
+    x = sin.(ω1*t)
+    y = cos.(ω2*t)
+    plot_data =[scatter(;x=x, y=y, mode="lines", name="Solution")] 
+    plot(plot_data, layout)
+end
+
+
+mathjax = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML"
+app = dash(external_scripts=[mathjax])
+app.title = "DashLissajous"
+
+curve = myplot(1,1)
+
+curve_graph = dcc_graph(id="graph",figure=curve)
+
+slider1 = dcc_slider(id="slider1",min=1, max=10,step=.01,value=1, marks=Dict([i => ("$(i)") for i = 1:10]))
+
+slider2 = dcc_slider(id="slider2",min=1,max=10, step=.01,value=1, marks=Dict([i => ("$(i)") for i = 1:10]))
+
+# App Layout
+app.layout = html_div(
+    [
+        html_center(html_h1("Lissajous Curves")),
+        html_center(curve_graph),
+        html_center(html_div(style=Dict("width"=>"420px"),[slider1,html_p(raw"\(\omega_1\)"),slider2, html_p(raw"\(\omega_2\)")]))
+    ]
+)
+ω1_old = 1
+ω2_old = 1
+callback!(app,Output("graph","figure"), Input("slider1","value"), Input("slider2","value")) do ω1, ω2
+    println(string(ω1_old)*","*string(ω2_old))
+    global ω1_old, ω2_old
+    ω1_old = ω1
+    ω2_old = ω2
+    myplot(ω1,ω2)
+end
+
+run_server(app, "0.0.0.0", debug=true)
